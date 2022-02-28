ed0c1cc6ec27ba8dfd3c656b9d1ccadee3d204e0
zhuzhe <1498852723@qq.com>
2022-02-28 23:55:42 +0800

    读取配置文件

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -1,10 +1,50 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let config = get_config("./App.toml");
+        
+        assert_eq!(String::from("config1"), config.package.name);
     }
 }
-use std::fs::File;
+use std::{fs::File, io::Read};
+use serde::Deserialize;
+
+#[derive(Deserialize,Debug)]
+pub struct Config {
+    package : Package
+}
+
+#[derive(Deserialize,Debug)]
+pub struct Package {
+    name: String,
+    version: String,
+    edition: Option<String>,
+}
 
 
+pub fn get_config(path: &'static str)-> Config{
+    let mut config_file = match File::open(path) {
+        Ok(c) => c,
+        Err(e) => {
+            panic!("未找到配置文件{}:{}", path,e);
+        }
+    };
+    let mut config_content = String::new();
+    match config_file.read_to_string(&mut config_content) {
+        Ok(c) => c,
+        Err(e) => {
+            panic!("读取失败:{}", e);
+        }
+    };
+    println!("文件内容:{}", config_content);
+    match toml::from_str(&config_content) {
+        Ok(c) => c,
+        Err(e) => {
+            panic!("toml文件解析失败:{}", e);
+        }
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,3 @@
-use std::{fs::File, io::Read};
-use serde::Deserialize;
-
-#[derive(Deserialize,Debug)]
-struct Config {
-    package : Package
-}
-
-#[derive(Deserialize,Debug)]
-struct Package {
-    name: String,
-    version: String,
-    edition: String,
-}
 fn main() {
-    let path = "./config/App.toml";
-    let mut config_file = match File::open(path) {
-        Ok(c) => c,
-        Err(e) => {
-            panic!("未找到配置文件{}:{}", path,e);
-        }
-    };
-    let mut config_content = String::new();
-    match config_file.read_to_string(&mut config_content) {
-        Ok(c) => c,
-        Err(e) => {
-            panic!("读取失败:{}", e);
-        }
-    };
-    println!("文件内容:{}", config_content);
-    let config:Config = toml::from_str(&config_content).unwrap();
-
-    println!("config:{:?}", config);
+    
 }
